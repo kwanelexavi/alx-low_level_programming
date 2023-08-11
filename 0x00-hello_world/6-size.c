b88b119e761a98820e5dd38ed2442a1ee31bbc80
kwanelexavi <kwanelexavi@gmail.com>
2023-08-10 18:55:06 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- *  main - A program that print the type and size of varaibles
+ *  main - A programe that print  the size of variable types.
  *  Return: 0 (success)
  */
 int main(void)
@@ -10,10 +10,9 @@ int main(void)
 	long int c;
 	long long int d;
 	float f;
-printf("Size of a char: %lu byte(s)\n", sizeof(a));
-printf("Size of a int: %lu byte(s)\n", sizeof(b));
-printf("Size of a long int: %lu byte(s)\n", sizeof(c));
-printf("Size of a long long int: %lu byte(s)\n", sizeof(d));
-printf("Size of a float: %lu byte(s)\n", sizeof(f));
+printf("Size of a char: %1u byte(s)\n", sizeof(a));
+printf("Size of a int: %1u byte(s)\n", sizeof(b));
+printf("Size of a long int: %1u byte(s)\n", sizeof(c));
+printf("Size of a long long int: %1u byte(s)\n", sizeof(d));
+printf("Size of a float: %1u byte(s)\n", sizeof(f));
 return (0);
-}
