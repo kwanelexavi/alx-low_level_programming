feffa3ded865705bc9ddc1926b46b86f668cc0a4
kwanelexavi <kwanelexavi@gmail.com>
2023-08-11 19:21:19 -0700

    /**  * main - Entry point  *  * Return: Always 0 (Success)
    git commit -m /**

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = '0';
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		int j = '0';
+		for (j = '0'; j <= '9'; j++)
+		{
+			if (!((i == j) || (j > i)))
+			{
+				putchar(j);
+				putchar(i);
+                                if (!(i == '9' && j == '8'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
