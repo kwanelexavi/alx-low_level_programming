eb7cf8173d791d2331a7b37f48ec0de65a815221
kwanelexavi <kwanelexavi@gmail.com>
2023-08-15 17:24:40 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* print_alphabet_x10 - print all alphabet in lowercase x10
+*/
+void print_alphabet_x10(void)
+{
+	char c;
+	int i = 0;
+
+	while (i <= 9)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+		i++;
+	}
+}
