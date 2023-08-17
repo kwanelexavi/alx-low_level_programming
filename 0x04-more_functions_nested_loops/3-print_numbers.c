76613cb7c7abc5c6427d28778210ae8f9b55d680
kwanelexavi <kwanelexavi@gmail.com>
2023-08-17 04:41:57 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers 0-9
+ * return: void
+ */
+
+void print_numbers(void)
+{
+	char num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		_putchar(num);
+	}
+	_putchar('\n');
+}
