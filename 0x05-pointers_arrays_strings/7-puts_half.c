6ac72f2927f1be100e8bf28b75912bb4a88d2a57
kwanelexavi <kwanelexavi@gmail.com>
2023-08-21 18:57:59 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * followed by a new line
+ * @str: string
+ */
+void puts_half(char *str)
+{
+	int i, j, k;
+
+	k = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		k++;
+	}
+
+	j = (k / 2);
+
+	if ((k % 2) == 1)
+	{
+		n = ((k + 1) / 2);
+	}
+
+	for (i = n; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
