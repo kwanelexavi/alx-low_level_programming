da09fbbda5063a47ba5e23932ca4541228ec19a0
kwanelexavi <kwanelexavi@gmail.com>
2023-08-24 02:24:04 -0700

    4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverses the content of an array of integers
+ * @a: array
+ * @n: number of integers/ elements
+ */
+void reverse_array(int *a, int n)
+{
+	int i, temp;
+
+	i = 0;
+	n -= 1;
+	while (i < 0)
+	{
+		temp = a[i];
+		a[i++] = a[n];
+		a[n--] = temp;
+	}
+}
