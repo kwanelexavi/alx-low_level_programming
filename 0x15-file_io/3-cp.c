f1302ec0c236cce0990b985a3e259c39d4cbfee6
kwanelexavi <kwanelexavi@gmail.com>
2023-10-02 16:00:03 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -28,7 +28,7 @@ char *create_buffer(char *file)
 
 /**
  * close_file - Closes file descriptors.
- * @fd: The file descriptor to be closed.
+ * @f: The file descriptor to be closed.
  */
 void close_file(int f)
 {
@@ -94,4 +94,3 @@ int main(int argc, char *argv[])
 
 	return (0);
 }
-
